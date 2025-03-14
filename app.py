fa742a4e36a9561cfdc8e74e20143f462c41b9f8
leowrs <1455586908@qq.com>
2025-03-15 06:06:26 +0800

    用户注册api已跑通

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from backend import create_app
+from flask_migrate import Migrate
+from backend import create_app, db
 
 app = create_app()
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     app.run(debug=True)  # 调试模式，自动重启代码修改
--- backend/models.py
+++ backend/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))  # 存储加密后的密码
+    password_hash = db.Column(db.String(512))  # 存储加密后的密码
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     # 加密密码的方法
